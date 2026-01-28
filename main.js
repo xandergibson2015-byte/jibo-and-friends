cd82d6cee8f2d8ff74a66468163e2337ab1b334d
xandergibson2015-byte <xandergibson2015@gmail.com>
2026-01-27 23:12:09 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
-    height: 800,
+    height: 700,
     webPreferences: {
-      preload: __dirname + "/preload.js"
+      contextIsolation: true
     }
   });
 
@@ -13,3 +14,9 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
